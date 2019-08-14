de8e6fbe14492e74c1930340d3eb04ca6f3e4115
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2019-08-13 22:18:35 -0400

    u

--- Examples/Week1.Examples.R
+++ Examples/Week1.Examples.R
@@ -1,7 +1,23 @@
 # Week 1 Examples
 library(tidyverse)
 library(readxl)
+library(janitor)
 library(tidylog)
 
 #VDOE Cohort Data
-cohort <- read_csv("http://www.doe.virginia.gov/statistics_reports/research_data/data_files/on-time-grad_cohort/2018-cohort.csv")
+cohort <- read_csv("http://www.doe.virginia.gov/statistics_reports/research_data/data_files/on-time-grad_cohort/2018-cohort.csv") %>%
+  clean_names() %>%
+  mutate(full.cohort = if_else(
+    is.na(gender) & is.na(disability_flag) & is.na(lep_flag) & is.na(disadvantaged_flag) & is.na(federal_race_code) & level_code == "DIV", 1, 0)) %>%
+  filter(full.cohort == 1)
+
+dropout <- cohort %>%
+  ggplot(aes(dropout_rate)) + 
+  geom_histogram()
+dropout
+
+diploma <- cohort %>%
+  ggplot(aes(diploma_rate)) +
+  geom_histogram()
+diploma
+

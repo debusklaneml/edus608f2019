737b7a89d31cb9fa1ccfe1800269773679510afb
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2019-08-13 21:56:13 -0400

    u

---
+++ Examples/Week1.Examples.R
@@ -0,0 +1,7 @@
+# Week 1 Examples
+library(tidyverse)
+library(readxl)
+library(tidylog)
+
+#VDOE Cohort Data
+cohort <- read_csv("http://www.doe.virginia.gov/statistics_reports/research_data/data_files/on-time-grad_cohort/2018-cohort.csv")

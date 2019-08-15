d0730eecc889c69c4e112c25569b09b045a3eab2
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2019-08-14 22:13:28 -0400

    u

--- Examples/Week1.Examples.R
+++ Examples/Week1.Examples.R
@@ -6,9 +6,12 @@ library(tidylog)
 
 #VDOE Cohort Data
 cohort <- read_csv("http://www.doe.virginia.gov/statistics_reports/research_data/data_files/on-time-grad_cohort/2018-cohort.csv") %>%
+  #clean up the variable names
   clean_names() %>%
+  #create a new variable "full.cohort" that indicates "1" if all of the other identifying variables are missing/NA
   mutate(full.cohort = if_else(
     is.na(gender) & is.na(disability_flag) & is.na(lep_flag) & is.na(disadvantaged_flag) & is.na(federal_race_code) & level_code == "DIV", 1, 0)) %>%
+  #keep/filter for only those that do not have subsample indications
   filter(full.cohort == 1)
 
 dropout <- cohort %>%

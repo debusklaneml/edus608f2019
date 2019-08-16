0c851f7e1798c8584c1d450efe75d4c4971c175a
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2019-08-16 13:27:01 -0400

    u

--- Examples/Week1.Examples.R
+++ Examples/Week1.Examples.R
@@ -4,6 +4,7 @@ library(readxl)
 library(janitor)
 library(tidylog)
 library(DescTools)
+library(fGarch)
 #VDOE Cohort Data
 cohort <- read_csv("http://www.doe.virginia.gov/statistics_reports/research_data/data_files/on-time-grad_cohort/2018-cohort.csv") %>%
   #clean up the variable names
@@ -27,3 +28,31 @@ diploma
 skimr::skim(cohort)
 Hmisc::describe(cohort)
 summarytools::freq(cohort$cohort_cnt)
+
+#distributions
+df <- data_frame(
+  skewed_normal = rsnorm(n = 1000, mean = 0, sd = 18, xi = 130),
+  exp_distrib = rexp(n = 1000, rate = .1),
+  gamma_distrib = rgamma(n = 1000, shape = 2, scale = 2),
+  beta_distrib = rbeta(n = 1000, shape1 = 4, shape2 = 2))
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+df %>% 
+  gather(key = distribution, value = value) %>% 
+  dplyr::filter(distribution == "exp_distrib") %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram(aes(y=..density..), colour="black", fill="white") +
+  #geom_density(alpha = .2, fill="#FF6666") +
+  #facet_wrap(~distribution) +
+  scale_fill_manual(values = c("red", "green", "blue", "orange")) +
+  scale_color_manual(values = c("red", "green", "blue", "orange")) +
+  labs(title = "Histogram of random draws from a Beta distribution") +
+  geom_vline(aes(xintercept = mean(value)), color = "red", linetype = "dashed", size = 1) +
+  geom_vline(aes(xintercept = getmode(value)), color = "blue") +
+  geom_vline(aes(xintercept = median(value)), color = "blue", size = 1)
+
+
